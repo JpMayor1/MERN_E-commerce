fecd04539b1af3f688257978eb88508a589ff1b3
James Phillip Mayor <117265110+JpMayor1@users.noreply.github.com>
2023-03-09 18:02:49 +0800

    Admin model created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Admin = require("../models/Admin");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -17,6 +18,7 @@ const createToken = (user) => {
     return token;
 };
 
+////////////////////////////////USER AUTH
 authController.post("/register", async (req, res) => {
     try {
         const isExisting = await User.findOne({ email: req.body.email });
@@ -66,6 +68,63 @@ authController.post("/login", async (req, res) => {
 
         const { password, ...others } = user._doc;
         const token = createToken(user);
+
+        return res.status(200).json({ others, token });
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+});
+
+////////////////////////////////ADMIN AUTH
+authController.post("/admin/register", async (req, res) => {
+    try {
+        const isExisting = await Admin.findOne({ email: req.body.email });
+        if (isExisting) {
+            return res.status(500).json({ msg: "Admin already registered" });
+        }
+        if (
+            req.body.username === "" ||
+            req.body.email === "" ||
+            req.body.password === ""
+        ) {
+            return res.status(500).json({ msg: "All fields are required" });
+        }
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const admin = await Admin.create({
+            ...req.body,
+            password: hashedPassword,
+        });
+        await admin.save();
+
+        const { password, ...others } = admin._doc;
+        const token = createToken(others);
+        return res.status(201).json({ others, token });
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+});
+
+authController.post("/admin/login", async (req, res) => {
+    const { email, password: adminPass } = req.body;
+    try {
+        if (email === "" || adminPass === "") {
+            return res.status(500).json({ msg: "all fields are required" });
+        }
+
+        const admin = await Admin.findOne({ email });
+        if (!admin) {
+            return res.status(500).json({ msg: "invalid email or password" });
+        }
+
+        const comparePassword = await bcrypt.compare(adminPass, admin.password);
+        if (!comparePassword) {
+            return res.status(500).json({ msg: "invalid email or password" });
+        }
+
+        const { password, ...others } = admin._doc;
+        const token = createToken(admin);
 
         return res.status(200).json({ others, token });
     } catch (error) {
