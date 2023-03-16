3861fdef6ce99d322a68587f96bfcba4c26511c3
James Phillip Mayor <117265110+JpMayor1@users.noreply.github.com>
2023-03-16 18:44:36 +0800

    delete handling success

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import axios from "axios";
 import "./ProductCard.css";
+import { toast } from "react-toastify";
 
 function ProductCard({ product }) {
+    const handleDelete = async () => {
+        try {
+            const res = await axios.delete(
+                `http://localhost:5000/product/${product._id}`
+            );
+            console.log(res.data.msg);
+            toast.success("Product Deleted", {
+                position: "top-center",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                onClose: () => {
+                    window.location.reload();
+                },
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
     return (
         <div className="product-container">
             <div className="wrapper">
@@ -16,6 +41,9 @@ function ProductCard({ product }) {
                         <span>₱</span>
                         {Number(product.price).toFixed(2)}
                     </span>
+                    <button className="delete-btn" onClick={handleDelete}>
+                        Delete
+                    </button>
                 </div>
             </div>
         </div>
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,6 @@
 const verifyToken = require("../middlewares/auth");
 const Product = require("../models/Product");
+const fs = require("fs");
 const productController = require("express").Router();
 
 //get all products
@@ -39,6 +40,26 @@ productController.post("/", verifyToken, async (req, res) => {
         await product.save();
 
         return res.status(201).json(product);
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+});
+
+//delete a product
+productController.delete("/:id", async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) {
+            return res.status(404).json({ msg: "No product found" });
+        }
+
+        // Delete image file from public/images
+        const imagePath = `public/images/${product.image}`;
+        if (fs.existsSync(imagePath)) {
+            fs.unlinkSync(imagePath);
+        }
+
+        return res.status(200).json({ msg: "Product deleted successfully" });
     } catch (error) {
         return res.status(500).json(error.message);
     }
